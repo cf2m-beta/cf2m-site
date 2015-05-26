2832e859280766ec42706ea0a7c8ad99363ca1f8
andrei <andrei.pastrama@gmail.com>
2015-05-26 13:52:25 +0200

    ajout menu déroulant + css

--- include/header.php
+++ include/header.php
@@ -1,6 +1,5 @@
 <div id="header">
     <img src="../images/new logo" alt="CF2M" title="CF2M">
-
     <a href="plan.php" >Plan du site</a>
     <a href="contact.php" >Contactez-nous</a>
 
@@ -8,10 +7,33 @@
         <input type="text">
         <input type="submit" value="Rechercher">
     </form>
+
+    <ul id="menu-deroulant">
+        
+        <li><a href="#">Formation</a>
+            <ul>
+                <li><a href="">Web développeur</a></li>
+                <li><a href="">Opérateur PAO / Infographiste</a></li>
+                <li><a href="">Animateur multimédia</a></li>
+                <li><a href="">Aide comptable</a></li>
+                <li><a href="">Technicien reseaux</a></li>
+            </ul>
+        </li>
+        
+        <li><a href="#">Infos</a>
+            <ul>
+                <li><a href="#">Inscription</a></li>
+                <li><a href="#">Test</a></li>
+                <li><a href="#">Horaire</a></li>
+            </ul>
+        </li>
+        
+        <li><a href="#">Media</a>
+            <ul>
+                <li><a href="#">Galerie</a></li>
+                <li><a href="#">Video</a></li>
+            </ul>
+        </li>
+    </ul>
     
-    <nav>
-        <a href="?menu=formations">Formations</a>
-        <a href="?menu=infos">Infos</a>
-        <a href="?menu=medias">Medias</a>
-    </nav>
 </div>
