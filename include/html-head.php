c0ff06834a1e37a78296e543cf0d52b22b241572
Eric Salla <sallartistee@yahoo.fr>
2015-07-01 12:10:33 +0200

    Avant_midi
    Ajout

--- include/html-head.php
+++ include/html-head.php
@@ -6,6 +6,7 @@
         <link rel="stylesheet" href="<?php print RACINE.STYLES_V1_CSS?>"/>
         <link href='http://fonts.googleapis.com/css?family=Questrial' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
         <link rel="shortcut icon" href="<?php print RACINE ?>images/icones/ico.ico" type="image/x-icon" />
         <title></title>
     </head>
