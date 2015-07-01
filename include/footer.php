942f703abc170df015b96e0638e1a4e37e1781f4
Eric Salla <sallartistee@yahoo.fr>
2015-07-01 16:17:53 +0200

    fin de la journée
    A la maison

--- include/footer.php
+++ include/footer.php
@@ -23,11 +23,11 @@
                <section>
                   <h4>Nos Formations</h4> 
                   <ul>
-                     <li><a href="<?php echo RACINE.'demploye/?menu=5' ?>">P.A.O</a></li> 
-                     <li><a href="<?php echo RACINE.'demploye/?menu=7' ?>">Aide-comptable</a></li>
+                     <li><a href="<?php echo RACINE.'demploye/?menu=7' ?>">Aide comptable</a></li>
                      <li><a href="<?php echo RACINE.'demploye/?menu=4' ?>">Développeur Web</a></li>
-                     <li><a href="<?php echo RACINE.'demploye/?menu=8' ?>">Technicien-Réseaux</a></li> 
-                     <li><a href="<?php echo RACINE.'demploye/?menu=6' ?>">Animateur MultiMédia</a></li>
+                     <li><a href="<?php echo RACINE.'demploye/?menu=5' ?>">Infographiste PAO</a></li> 
+                     <li><a href="<?php echo RACINE.'demploye/?menu=8' ?>">Technicien Réseau</a></li> 
+                     <li><a href="<?php echo RACINE.'demploye/?menu=6' ?>">Animateur Multimédia</a></li>
                   </ul>
                </section>
                
