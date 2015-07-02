1c9e96c69a47371b889777ca71239761e50366a7
lumierox <lumierox@andrei-notebook.intracf2m.be>
2015-07-02 10:48:02 +0200

    correction bugs+ base de données

--- include/html-head.php
+++ include/html-head.php
@@ -9,6 +9,6 @@
         <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
         <link rel="shortcut icon" href="<?php print RACINE ?>images/icones/ico.ico" type="image/x-icon" />
         <link rel="stylesheet" type="text/css" href="<?php print RACINE ?>slider/engine1/style.css" />
-	
+	<script src="<?php print RACINE?>js/jquery-1.9.1.min.js"></script>
         <title></title>
     </head>
