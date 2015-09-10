b933b1776f94c3c0a81821cec10f9183325229f2
Eric Salla <sallartistee@yahoo.fr>
2015-09-10 16:07:32 +0200

    Commit du  sept septembre

--- include/html-head.php
+++ include/html-head.php
@@ -7,7 +7,6 @@
         <link href='http://fonts.googleapis.com/css?family=Questrial' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-        <link href='https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz' rel='stylesheet' type='text/css'>
         <link href='https://fonts.googleapis.com/css?family=Raleway:600' rel='stylesheet' type='text/css'>
         <link rel="shortcut icon" href="<?php print RACINE ?>images/icones/ico.ico" type="image/x-icon" />
         <link rel="stylesheet" type="text/css" href="<?php print RACINE ?>slider/engine1/style.css" />
